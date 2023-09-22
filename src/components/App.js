04b87302343b70849373e18946957ad0b5f3e908
Disha <bidisha2125@gmail.com>
2023-09-22 20:45:29 +0530

    Created the points logic

--- src/components/App.js
+++ src/components/App.js
@@ -6,54 +6,62 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 
-
 const initialState = {
   questions: [],
 
   //loading, error, ready, active, finished
-  status: 'loading',
+  status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
-    case 'dataReceived':
+    case "dataReceived":
       return {
         ...state,
         questions: action.payload,
-        status: 'ready',
-      }
-    case 'dataFailed':
+        status: "ready",
+      };
+    case "dataFailed":
       return {
         ...state,
-        status: 'error',
-      }
-    case 'start':
+        status: "error",
+      };
+    case "start":
       return {
         ...state,
-        status: 'active',
-      }
-    case 'newAnswer':
+        status: "active",
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
       return {
-          ...state, 
-          answer: action.payload,
-      }
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
-      throw new Error("Unknown action")
+      throw new Error("Unknown action");
   }
 }
 
 export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
-
-  const numQuestions = questions.length
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
-      .catch((err) => dispatch({ type: 'dataFailed' }));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
@@ -63,8 +71,16 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question question={questions[index]} dispatch = {dispatch} answer={answer} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
